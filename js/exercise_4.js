4b69b7b192473ab231657e8feb8ef7eba7f4a9c6
Brendan Harmon <brendan.harmon@gmail.com>
2016-05-02 15:25:19 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -57,3 +57,8 @@ featureLayerParks.on('ready', function(){
     })
 })
 
+featureLayerParks.on('ready', function(){
+  this.eachLayer(function(layer){
+  	layer.bindPopup('Welcome to '+ layer.feature.properties.name);
+  })
+})
