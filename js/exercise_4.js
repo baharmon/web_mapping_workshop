653e2a52ec410792a1a9439e50506334b77ca314
baharmon <baharmon@ncsu.edu>
2016-05-02 15:55:34 -0400

    update

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -54,15 +54,46 @@ var clickHandler = function(e){
     }
     info += '</div>';
     $('#info').append(info);
-  })
-}
+  });
+};
 
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
   	layer.on('click', clickHandler);
-  })
-})
+  });
+});
 
+// COMPARE
+var clickHandler = function(e){
+  $('#info').empty();
+
+  var feature = e.target.feature;
+
+  $('#info').fadeIn(400,function(){
+    var info = '';
+
+    info += '<div>'
+    info += '<h2>' + feature.properties.name + '</h2>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.cuisine + '</p>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.phone + '</p>'
+    if(feature.properties.phone) info +=   '<p>'  + feature.properties.website + '</p>'
+    if(feature.properties.phone) info +=   '<p><a href="' + feature.properties.website + '">'  + feature.properties.website + '</a></p>'
+    info += '</div>'
+
+    $('#info').append(info);
+  });
+};
+
+featureLayer.on('ready', function(){
+  this.eachLayer(function(layer){
+    layer.on('click', clickHandler);
+  });
+});
+
+map.on('click',function(e){
+	$('#info').fadeOut(200);
+    $('#info').empty();
+});
 
 
 
