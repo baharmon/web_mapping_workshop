6cea41419eeb151f305d8281e3f69d9746e4b7d8
baharmon <baharmon@ncsu.edu>
2016-05-02 16:27:35 -0400

    update

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -68,7 +68,7 @@ map.on('click',function(){
 var myLocation = L.mapbox.featureLayer().addTo(map);
 
 map.on('locationfound', function(e){
-	myLocation,setGeoJSON({
+	myLocation.setGeoJSON({
 		type: 'Feature',
 		geometry: {
 			type: 'Point',
